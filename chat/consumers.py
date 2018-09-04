4d10240ad0ec878505f20df95e0813eafcc152c5
sergiy-chumachenko <chumachenko.sergiy@gmail.com>
2018-09-04 23:37:27 +0300

    [UPDATE] websocket connect method updated

--- chat/consumers.py
+++ chat/consumers.py
@@ -10,9 +10,27 @@ from chat.models import Thread, ChatMessage
 class ChatConsumer(AsyncConsumer):
     async def websocket_connect(self, event):
         print("connected", event)
+        await self.send({
+            "type": "websocket.accept"
+        })
+        other_user = self.scope['url_route']['kwargs']['username']
+        me = self.scope['user']
+        print(other_user, me)
+        thread_obj = await self.get_thread(me, other_user)
+        print(thread_obj)
+        # await asyncio.sleep(10)
+        await self.send({
+            "type": "websocket.send",
+            "text": "Hello World"
+        })
 
     async def websocket_receive(self, event):
         print("receive", event)
 
     async def websocket_disconnect(self, event):
         print("disconnected", event)
+
+    @database_sync_to_async
+    def get_thread(selfself, user, other_username):
+        return Thread.objects.get_or_new(user, other_username)[0]
+
