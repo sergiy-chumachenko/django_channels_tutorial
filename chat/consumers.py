ab130c33006673bbcdffc65afb2ee89a03fcd687
sergiy-chumachenko <chumachenko.sergiy@gmail.com>
2018-09-06 23:55:58 +0300

    [UPDATE] consumers updated + redis

--- chat/consumers.py
+++ chat/consumers.py
@@ -10,27 +10,57 @@ from chat.models import Thread, ChatMessage
 class ChatConsumer(AsyncConsumer):
     async def websocket_connect(self, event):
         print("connected", event)
+        other_user = self.scope['url_route']['kwargs']['username']
+        me = self.scope['user']
+        thread_obj = await self.get_thread(me, other_user)
+        chat_room = "thread_{}".format(thread_obj.id)
+        self.chat_room = chat_room
+        self.thread_obj = thread_obj
+        await self.channel_layer.group_add(
+            chat_room,
+            self.channel_name
+        )
         await self.send({
             "type": "websocket.accept"
         })
-        other_user = self.scope['url_route']['kwargs']['username']
-        me = self.scope['user']
-        print(other_user, me)
-        thread_obj = await self.get_thread(me, other_user)
-        print(thread_obj)
         # await asyncio.sleep(10)
-        await self.send({
-            "type": "websocket.send",
-            "text": "Hello World"
-        })
+
 
     async def websocket_receive(self, event):
-        print("receive", event)
+        front_text = event.get('text', None)
+        if front_text is not None:
+            loaded_dict_data = json.loads(front_text)
+            msg = loaded_dict_data.get('message')
+            user = self.scope['user']
+            username = 'default'
+            if user.is_authenticated:
+                username = user.username
+                await  self.create_chat_message(user, msg)
+            myresponse = {
+                "message": msg,
+                "username": username
+            }
+            await self.channel_layer.group_send(
+                self.chat_room,
+                {
+                    "type": "chat_message",
+                    "text": json.dumps(myresponse)
+                }
+            )
+
+    async def chat_message(self, event):
+        await self.send({
+            "type": "websocket.send",
+            "text": event['text']
+        })
 
     async def websocket_disconnect(self, event):
         print("disconnected", event)
 
     @database_sync_to_async
-    def get_thread(selfself, user, other_username):
+    def get_thread(self, user, other_username):
         return Thread.objects.get_or_new(user, other_username)[0]
 
+    @database_sync_to_async
+    def create_chat_message(self, me, msg):
+        return ChatMessage.objects.create(thread=self.thread_obj, user=me, message=msg)
--- django_channels_tutorial/settings.py
+++ django_channels_tutorial/settings.py
@@ -54,6 +54,16 @@ MIDDLEWARE = [
 ROOT_URLCONF = 'django_channels_tutorial.urls'
 ASGI_APPLICATION = 'django_channels_tutorial.routing.application'
 
+CHANNEL_LAYERS = {
+    'default': {
+        'BACKEND': 'channels_redis.core.RedisChannelLayer',
+        'CONFIG': {
+            "hosts": [('127.0.0.1', 6379)],
+            # "hosts": [os.environ.get('REDIS_URL', 'redis://localhost:6379')],
+        },
+    },
+}
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
